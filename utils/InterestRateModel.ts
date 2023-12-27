9390c6e7a8dbe51643003b589ed00072f5aa84b0
itofarina <farinalvaro@gmail.com>
2023-12-27 16:34:45 -0300

    ✨ irm: new floating formula

---
+++ utils/InterestRateModel.ts
@@ -0,0 +1,39 @@
+import { WAD, expWad, lnWad } from './FIxedPointMathLib';
+
+export type FloatingParameters = {
+  a: bigint;
+  b: bigint;
+  floatingNaturalUtilization: bigint;
+  maxUtilization: bigint;
+  sigmoidSpeed: bigint;
+  growthSpeed: bigint;
+  maxRate: bigint;
+};
+
+export type FixedParameters = FloatingParameters & {
+  spreadFactor: bigint;
+  timePreference: bigint;
+  maturitySpeed: bigint;
+};
+
+export function floatingRate(parameters: FloatingParameters, uFloating: bigint, uGlobal: bigint): bigint {
+  const { a, b, maxUtilization, floatingNaturalUtilization, sigmoidSpeed, growthSpeed, maxRate } = parameters;
+
+  const r = (a * WAD) / (maxUtilization - uFloating) + b;
+  if (uGlobal === WAD) return maxRate;
+  if (uGlobal === 0n) return r;
+  if (uGlobal >= uFloating) {
+    const sig =
+      (WAD * WAD) /
+      (WAD +
+        expWad(
+          (-sigmoidSpeed *
+            (lnWad((uGlobal * WAD) / (WAD - uGlobal)) -
+              lnWad((floatingNaturalUtilization * WAD) / (WAD - floatingNaturalUtilization)))) /
+            WAD,
+        ));
+    const rate = (expWad((-growthSpeed * lnWad(WAD - (sig * uGlobal) / WAD)) / WAD) * r) / WAD;
+    return rate > maxRate ? maxRate : rate;
+  }
+  return r;
+}
